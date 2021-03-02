8ed72f6416a03bff63eb65a1ea60542ef76bf553
Alejandro Menocal <alejandro.menocal@exxonmobil.com>
2021-03-01 22:18:37 -0600

    adding GradeBook

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        //this is a field
+        private List<double> grades = new List<double>();
+        private string name;
+
+
+    }
+
+}
