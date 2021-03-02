6ece8121559925a3461cc9dea7dbbac7a699f393
Alejandro Menocal <alejandro.menocal@exxonmobil.com>
2021-03-01 22:32:30 -0600

    created show statistcs method for easier code reading

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -15,6 +15,25 @@ namespace GradeBook
             grades.Add(grade);
         }
 
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The Highest Grade is {highGrade}");
+            Console.WriteLine($"The Lowest Grade is {lowGrade}");
+        }
+
         //this is a field
         private List<double> grades = new List<double>();
         private string name;
